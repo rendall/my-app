6fef8a4813a6f66944a091a8a8c701d6ac97cbbf
Rendall <rendall@gmail.com>
2023-11-23 10:57:50 +0200

    WIP: new feature!

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Contact</title>
+	<meta name="description" content="Contanct info" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Contact info</h1>
+	<p>This is new contact info. 555-7392</p>
+</div>
