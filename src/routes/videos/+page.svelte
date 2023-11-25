c479d39c83d6bbed48e87163f854015a4e97bdd3
Rendall <rendall@gmail.com>
2023-11-25 11:32:31 +0200

    Add video links

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -7,10 +7,53 @@
 	<h1>Full Stack Videos</h1>
 
 	<section>
+		<header><h2>Unit Tests series (Part 1)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-000.mp4" type="video/mp4" />
+		</video>
+	</section>
+	<section>
+		<header><h2>Unit Tests series (Part 2)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-001.mp4" type="video/mp4" />
+		</video>
+	</section>
+	<section>
+		<header><h2>Unit Tests series (Part 3)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-002.mp4" type="video/mp4" />
+		</video>
+	</section>
+	<section>
+		<header><h2>Unit Tests series (Part 4)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-003.mp4" type="video/mp4" />
+		</video>
+	</section>
+	<section>
+		<header><h2>Unit Tests series (Part 5)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-004.mp4" type="video/mp4" />
+		</video>
+	</section>
+	<section>
+		<header><h2>Unit Tests series (Part 6)</h2></header>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video controls>
+			<source src="/videos/UnitTests-005.mp4" type="video/mp4" />
+		</video>
+	</section>
+
+	<section>
 		<header><h2>Finite State Machine as User Interface (addendum I)</h2></header>
 		<!-- svelte-ignore a11y-media-has-caption -->
 		<video controls>
-			<source src="/videos/FSMasUI-1.mp4" type="video/mp4">
+			<source src="/videos/FSMasUI-1.mp4" type="video/mp4" />
 		</video>
 	</section>
 
@@ -18,8 +61,7 @@
 		<header><h2>Finite State Machine as User Interface (addendum II)</h2></header>
 		<!-- svelte-ignore a11y-media-has-caption -->
 		<video controls>
-			<source src="/videos/FSMasUI-2.mp4" type="video/mp4">
+			<source src="/videos/FSMasUI-2.mp4" type="video/mp4" />
 		</video>
 	</section>
 </div>
-
